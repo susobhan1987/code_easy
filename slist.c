ad860322989489598c965302c65119bb1a49bd65
root <root@localhost.localdomain>
2016-07-18 14:59:44 -0500

    Add code to merge two sorted list

--- slist.c
+++ slist.c
@@ -109,6 +109,24 @@ void skipM_deleteN (struct node *head,int m, int n) {
 
 }
 
+struct node * merge_slist (struct node *a, struct node *b) {
+	struct node *result;
+	if (a == NULL) return b;
+	if (b == NULL) return a;
+
+
+	if (a->data < b->data) {
+
+		result = a;
+		result->next = merge_slist(a->next,b);
+	}
+	else {
+		result = b;
+		result->next = merge_slist (a, b->next);
+	}
+	return result;
+}
+
 int main () {
 	struct node *tmp = NULL;
 	createNode (&tmp,6);
